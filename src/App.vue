338592e0df1c3f5b276a52a31005bd378b9fe7d0
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-06-12 22:56:47 -0300

    vue on click

--- src/App.vue
+++ src/App.vue
@@ -7,21 +7,26 @@ export default defineComponent({
       users: [
         { name: 'user one', age: 12, isActive: false },
         { name: 'user two', age: 20, isActive: true },
-        { name: 'user three', age: 23, isActive: true }
-      ]
+        { name: 'user three', age: 23, isActive: true },
+      ],
+      randomNumber: 0
     }
   },
   methods: {
-
+    getRandomNumber() {
+      this.randomNumber = Math.ceil(Math.random() * 100)
+    }
   }
 })
 </script>
 
 <template>
   <section>
-    <li v-for="user in users" :class="{ active: user.isActive }" >
+    <li v-for="user in users" :class="{ active: user.isActive }">
       {{ user.name }}
     </li>
+    <div> {{ randomNumber }} </div>
+    <button @click="getRandomNumber">Get a random number</button>
   </section>
 </template>
 
